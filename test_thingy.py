ead0a91bd9d6a17827945799eee0397db040ffd0
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-03-21 00:10:57 +0100

    Improve test on classproperty

--- test_thingy.py
+++ test_thingy.py
@@ -6,11 +6,19 @@ from thingy import DatabaseThingy, Thingy, View, classproperty
 
 def test_classproperty():
     class Test:
+        _foo = "bar"
+
         @classproperty
         def foo(cls):
-            return "bar"
+            return cls._foo
+
+        @foo.setter
+        def set_foo(cls, value):
+            cls._foo = value
 
     assert Test.foo == "bar"
+    Test.foo = "baz"
+    assert Test.foo == "baz"
 
 
 def test_view_return_type():
