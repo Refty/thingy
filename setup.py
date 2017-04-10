63f9b304a3e594b043bd739de1e32e090a0028d0
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-10 15:05:10 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="Thingy",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/numberly/thingy",
     license="MIT",
     author="Guillaume Gelin",
