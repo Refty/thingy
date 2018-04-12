c2aeb7bb6c6a4540e68bc82d4f9e314ce1748a09
ramnes <contact@ramnes.eu>
2018-04-12 19:07:45 +0200

    Release 0.8.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="Thingy",
-    version="0.8.3",
+    version="0.8.4",
     url="https://github.com/numberly/thingy",
     license="MIT",
     author="Guillaume Gelin",
